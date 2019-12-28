c7184e6c380a7240f17ec28df3ba8d05757c4726
anatawa12 <anatawa12@icloud.com>
2019-12-28 18:23:09 +0900

    configure integration_tests project

---
+++ integration_tests/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
+plugins {
+    kotlin("jvm")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+configurations {
+    //testImplementation.get().extendsFrom(project(":").configurations.testImplementation.get())
+}
+
+sourceSets {
+    test {
+        withConvention(KotlinSourceSet::class){
+            kotlin.srcDir("test")
+        }
+
+        compileClasspath += project(":").sourceSets.test.get().output
+        runtimeClasspath += project(":").sourceSets.test.get().output
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+configurations {
+    testImplementation.get().extendsFrom(project(":").configurations.testImplementation.get())
+    testRuntimeOnly.get().extendsFrom(project(":").configurations.testRuntimeOnly.get())
+}
+
+dependencies {
+    testImplementation(kotlin("stdlib-jdk8"))
+    testCompile(project(":"))
+    compile("org.ow2.asm:asm-debug-all:5.2")
+    testImplementation("junit:junit:4.10")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 include("scene-lib")
+include("integration_tests")
