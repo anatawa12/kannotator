2a2abc1b63d760ce5dcc5f075aba5d68f9017a2c
anatawa12 <anatawa12@icloud.com>
2019-12-28 20:37:43 +0900

    testImplementation jung

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,10 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     compile("org.ow2.asm:asm-debug-all:5.2")
     implementation("junit:junit:4.10")
-    // jung
+    testImplementation("net.sf.jung:jung-graph-impl:2.0.1")
+    testImplementation("net.sf.jung:jung-algorithms:2.0.1")
+    testImplementation("net.sf.jung:jung-visualization:2.0.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     implementation("com.goldmansachs:gs-collections:4.1.0")
     implementation("com.google.guava:guava:13.0.1")
     implementation("org.jetbrains:annotations:18.0.0")
